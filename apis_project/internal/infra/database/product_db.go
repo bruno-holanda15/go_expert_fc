1b2aed480e81ce83ed437084613a33897c35b006
Bruno Git <br.holanda15@gmail.com>
2024-03-23 10:30:32 -0300

    feat: create FindAll

--- apis_project/internal/infra/database/product_db.go
+++ apis_project/internal/infra/database/product_db.go
@@ -46,3 +46,19 @@ func (p *Product) Delete(id string) error {
 
 	return p.DB.Delete(product).Error
 }
+
+func (p *Product) FindaAll(page, limit int, sort string) ([]entity.Product, error) {
+	var products []entity.Product
+	var err error
+	if sort != "" && sort != "asc" && sort != "desc"  {
+		sort = "asc"
+	}
+
+	if page != 0 && limit != 0 {
+		err = p.DB.Limit(limit).Offset((page-1) * limit).Order("created_at" + sort).Find(&products).Error
+	} else {
+		err = p.DB.Order("created_at").Find(&products).Error
+	}
+
+	return products, err
+}
