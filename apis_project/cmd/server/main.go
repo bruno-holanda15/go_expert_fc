5434a7c49913c4a8ae8c0a9a10e11dbe6d7651aa
Bruno Git <br.holanda15@gmail.com>
2024-03-25 08:28:05 -0300

    feat: starting handler structure

--- apis_project/cmd/server/main.go
+++ apis_project/cmd/server/main.go
@@ -1,9 +1,69 @@
 package main
 
-import "github.com/bruno-holanda15/go_expert_fc/APIs_project/configs"
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/bruno-holanda15/go_expert_fc/APIs_project/configs"
+	"github.com/bruno-holanda15/go_expert_fc/APIs_project/internal/dto"
+	"github.com/bruno-holanda15/go_expert_fc/APIs_project/internal/entity"
+	"github.com/bruno-holanda15/go_expert_fc/APIs_project/internal/infra/database"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 func main() {
-	config, _ := configs.LoadConfig(".")
-	println(config.DBDriver)
+	_, err := configs.LoadConfig(".")
+	if err != nil {
+		panic(err)
+	}
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&entity.User{}, &entity.Product{})
+
+	productDB := database.NewProductDB(db)
+	productHandler := NewProductHandler(productDB)
+
+	http.HandleFunc("/products", productHandler.CreateProduct)
+	http.ListenAndServe(":8001", nil)
 }
 
+type ProductHandler struct {
+	// usando interface para não injetar diretamente pacote ??
+	ProductDB database.ProductInterface
+}
+
+func NewProductHandler(db database.ProductInterface) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: db,
+	}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product dto.CreateProductInput
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// deveria estar dentro da camada application, como no usecase
+	p, err := entity.NewProduct(product.Name, product.Price)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// deveria estar dentro da camada application, como no usecase
+	err = h.ProductDB.Create(p)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+}
