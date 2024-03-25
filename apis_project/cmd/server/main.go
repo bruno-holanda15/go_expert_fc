94ed6687f7eca147c001430669e2813761a2f558
Bruno Git <br.holanda15@gmail.com>
2024-03-25 18:40:42 -0300

    feat: add webserver folder inside infra

--- apis_project/cmd/server/main.go
+++ apis_project/cmd/server/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/bruno-holanda15/go_expert_fc/APIs_project/configs"
-	"github.com/bruno-holanda15/go_expert_fc/APIs_project/internal/dto"
 	"github.com/bruno-holanda15/go_expert_fc/APIs_project/internal/entity"
 	"github.com/bruno-holanda15/go_expert_fc/APIs_project/internal/infra/database"
+	"github.com/bruno-holanda15/go_expert_fc/APIs_project/internal/infra/webserver"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
@@ -26,44 +25,8 @@ func main() {
 	db.AutoMigrate(&entity.User{}, &entity.Product{})
 
 	productDB := database.NewProductDB(db)
-	productHandler := NewProductHandler(productDB)
+	productHandler := webserver.NewProductHandler(productDB)
 
 	http.HandleFunc("/products", productHandler.CreateProduct)
 	http.ListenAndServe(":8001", nil)
-}
-
-type ProductHandler struct {
-	// usando interface para não injetar diretamente pacote ??
-	ProductDB database.ProductInterface
-}
-
-func NewProductHandler(db database.ProductInterface) *ProductHandler {
-	return &ProductHandler{
-		ProductDB: db,
-	}
-}
-
-func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
-	var product dto.CreateProductInput
-	err := json.NewDecoder(r.Body).Decode(&product)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	// deveria estar dentro da camada application, como no usecase
-	p, err := entity.NewProduct(product.Name, product.Price)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	// deveria estar dentro da camada application, como no usecase
-	err = h.ProductDB.Create(p)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(http.StatusCreated)
 }
