60f43ecb4e3e0b5ac520e2d4ba850c6b89adf162
Bruno Git <br.holanda15@gmail.com>
2024-04-21 10:54:58 -0300

    feat: solving concurrency problem with lib sync/atomic -> AddUint()

--- multithreading/concorrencias-mundo-real/main.go
+++ multithreading/concorrencias-mundo-real/main.go
@@ -3,20 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"sync"
+	"sync/atomic"
+
+	// "sync"
 	"time"
 )
 
 var number uint64 = 0
 
 func main() {
-	m := sync.Mutex{}
+	// m := sync.Mutex{}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		m.Lock()
-		number++
-		m.Unlock()
-		w.Write([]byte(fmt.Sprintf("Olá, você é o visitante número %d", number)))
+		// m.Lock()
+		// number++
+		atomic.AddUint64(&number, 1)
+		// m.Unlock()
 		time.Sleep(300 * time.Millisecond)
+		w.Write([]byte(fmt.Sprintf("Olá, você é o visitante número %d", number)))
 	})
 
 	http.ListenAndServe(":3000", nil)
