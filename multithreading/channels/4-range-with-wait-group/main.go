d552f59af607d73dfd4cc3d0888a8a092e2bbd37
Bruno Git <br.holanda15@gmail.com>
2024-04-22 09:21:17 -0300

    feat: develop publisher and consumer with channels and sync.WaitGroups{}

---
+++ multithreading/channels/4-range-with-wait-group/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+// Thread 1
+func main() {
+	ch := make(chan int)
+	
+	wg := sync.WaitGroup{}
+	wg.Add(10)
+	
+	go publisher(ch)
+	go consumer(ch, &wg)
+
+	wg.Wait()
+}
+
+func publisher(ch chan int) {
+	for i := 0; i < 10; i++ {
+		ch <- i
+	}
+	
+	close(ch)
+}
+
+func consumer(ch chan int, wg *sync.WaitGroup) {
+	for x := range ch {
+		fmt.Printf("Consumed ch and received ==> %d\n", x)
+		wg.Done()
+	}
+}
