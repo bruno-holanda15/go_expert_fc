d89aa1b8d4e2da21dd341d8e4fc92040a45021f0
Bruno Git <br.holanda15@gmail.com>
2024-04-22 09:14:12 -0300

    feat: develop publisher and consumer with channels

---
+++ multithreading/channels/3-range/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+// Thread 1
+func main() {
+	ch := make(chan int)
+	
+	go publisher(ch)
+	consumer(ch)
+}
+
+func publisher(ch chan int) {
+	for i := 0; i < 10; i++ {
+		ch <- i
+	}
+	
+	close(ch)
+}
+
+func consumer(ch chan int) {
+	for x := range ch {
+		fmt.Printf("Consumed ch and received ==> %d\n", x)
+	}
+}
