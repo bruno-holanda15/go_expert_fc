2aacea34e4e00717efa6a6b895333022cb7d4000
Bruno Git <br.holanda15@gmail.com>
2024-04-26 08:03:21 -0300

    refactor: connecting with database and creating category

--- cli_cobra/cmd/category.go
+++ cli_cobra/cmd/category.go
@@ -4,10 +4,10 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	// "fmt"
-
+	"cli_cobra/internal/database"
+	"database/sql"
 	"fmt"
-
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/spf13/cobra"
 )
 
@@ -16,32 +16,39 @@ var categoryCmd = &cobra.Command{
 	Use:   "category",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+and usage of using your command.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// cmd.Help()
-		color, _ := cmd.Flags().GetBool("color")
-		name, _ := cmd.Flags().GetString("name")
-		fmt.Println(color, name)
+		categoryDB := GetCategory(GetDB())
+		cat, err := categoryDB.Create(name, description)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(cat)
+		}
 	},
 }
 
-var category string
+var name string
+var description string
 
 func init() {
 	rootCmd.AddCommand(categoryCmd)
-	categoryCmd.PersistentFlags().StringVarP(&category, "name", "n", "", "write name pls")
-	categoryCmd.PersistentFlags().BoolP("color", "c", false, "change color")
-	// Here you will define your flags and configuration settings.
+	categoryCmd.Flags().StringVarP(&name, "name", "n", "", "Category name")
+	categoryCmd.Flags().StringVarP(&description, "description", "d", "", "Category description")
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// categoryCmd.PersistentFlags().String("foo", "", "A help for foo")
+	categoryCmd.MarkFlagRequired("name")
+	categoryCmd.MarkFlagRequired("description")
+}
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// categoryCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+func GetDB() *sql.DB {
+	db, err := sql.Open("sqlite3", "./data.db")
+	if err != nil {
+		panic(err)
+	}
+
+	return db
+}
+
+func GetCategory(db *sql.DB) *database.Category {
+	return database.NewCategory(db)
 }
