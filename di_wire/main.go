cc87106190ba2faebb776011ce15fc229cddded4
Bruno Git <br.holanda15@gmail.com>
2024-06-01 10:54:58 -0300

    feat: started di with wire

---
+++ di_wire/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"go_di_wire/product"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./test.db")
+	if err != nil {
+		panic(err)
+	}
+
+	repository := product.NewProductRepository(db)
+	usecase := product.NewProductUseCase(repository)
+
+	p, err := usecase.GetProductById(1)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(p.Name)
+}
